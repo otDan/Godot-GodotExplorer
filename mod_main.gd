cc8ba4ef118552f5cab897c3df6917c64d540474
otDan <otdanofficial@gmail.com>
2023-01-20 10:43:37 +0100

    Remove theme and support mod loader version 4.0

--- mod_main.gd
+++ mod_main.gd
@@ -5,12 +5,14 @@ const MOD_NAME = "otDan-GodotExplorer"
 var scene_navigator
 
 func _init(modLoader = ModLoader):
-	modLoader.mod_log("Init Godot Explorer.", MOD_NAME)
+	ModLoaderUtils.log_info("Init Godot Explorer.", MOD_NAME)
 	
 func _ready():
-	ModLoader.mod_log("Readying Godot Explorer.", MOD_NAME)
-	# Atache display to root
-	var scene_navigator_instance = load("res://mods-unpacked/otDan-GodotExplorerV1/SceneNavigator.tscn").instance()
+	ModLoaderUtils.log_info("Readying Godot Explorer.", MOD_NAME)
+	# Attach display to root
+	var scene_navigator_path = ModLoaderUtils.get_local_folder_dir("mods-unpacked" + "/otDan-GodotExplorer/SceneNavigator.tscn")
+	ModLoaderUtils.log_info(scene_navigator_path, MOD_NAME)
+	var scene_navigator_instance = load(scene_navigator_path).instance()
 	get_tree().root.call_deferred("add_child", scene_navigator_instance)
 
-	ModLoader.mod_log("Attached scene viewer to root", MOD_NAME)
+	ModLoaderUtils.log_info("Attached scene viewer to root", MOD_NAME)
