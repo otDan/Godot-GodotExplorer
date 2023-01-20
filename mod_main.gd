3cfdf2cd16b9d8baf5d475e402afab0065cad016
otDan <otdanofficial@gmail.com>
2023-01-20 10:44:02 +0100

    Remove debug line

--- mod_main.gd
+++ mod_main.gd
@@ -11,7 +11,6 @@ func _ready():
 	ModLoaderUtils.log_info("Readying Godot Explorer.", MOD_NAME)
 	# Attach display to root
 	var scene_navigator_path = ModLoaderUtils.get_local_folder_dir("mods-unpacked" + "/otDan-GodotExplorer/SceneNavigator.tscn")
-	ModLoaderUtils.log_info(scene_navigator_path, MOD_NAME)
 	var scene_navigator_instance = load(scene_navigator_path).instance()
 	get_tree().root.call_deferred("add_child", scene_navigator_instance)
 
